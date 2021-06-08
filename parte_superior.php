2efb7e7cc9c33c6ff659a9749c06a9cd5a7c6692
Orestes <44916083+Oreramirez@users.noreply.github.com>
2021-06-07 23:09:15 -0500

    logo actualizacion

--- parte_superior.php
+++ parte_superior.php
@@ -41,7 +41,7 @@
 
     <!-- Preloader -->
     <div class="preloader flex-column justify-content-center align-items-center">
-      <img class="animation__shake" src="dist/img/logoSigma.png" alt="" height="60" width="160">
+      <img class="animation__shake" src="dist/img/logoSigma.png" alt="" height="60" width="180">
     </div>
 
     <!-- Navbar -->
