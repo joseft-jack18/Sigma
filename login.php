dcb63466295e18a3684c144f05af58b3094516d4
Orestes <44916083+Oreramirez@users.noreply.github.com>
2021-06-10 20:51:09 -0500

    actualizacion login

--- login.php
+++ login.php
@@ -12,12 +12,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="shortcut icon" href="#" />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sistema de Liquidacion | Log in</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Boostrap -->
+    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="plugins/sweetalert2/sweetalert2.min.css">        
+    <link rel="stylesheet" type="text/css" href="fuentes/iconic/css/material-design-iconic-font.min.css">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
     <!-- icheck bootstrap -->
@@ -28,8 +34,8 @@
     <link rel="stylesheet" href="plugins/toastr/toastr.min.css">
 </head>
 
-<body class="hold-transition login-page">
-    <div class="login-box">
+<body class="container-login">
+    <div class="wrap-login">
         <div class="login-logo">
             <a href="index2.html"><b>Sistema</b>Liquidaciones</a>
         </div>
@@ -39,12 +45,13 @@
             <div class="card-body login-card-body">
                 <p class="login-box-msg">Regístrese para iniciar su sesión</p>                
 
-                <form action="ajax/login_action.php" method="post">
+                <form class="login-form validate-form" action="ajax/login_action.php" method="post">
                     <div class="input-group mb-3">
                         <input type="text" class="form-control" id="txt_usuario" name="txt_usuario" placeholder="Usuario">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-user"></span>
+                                <span class="focus-efecto"></span>
                             </div>
                         </div>
                     </div>
@@ -53,6 +60,7 @@
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
+                                <span class="focus-efecto"></span>
                             </div>
                         </div>
                     </div>
@@ -67,7 +75,7 @@
                         </div>
                         <br><br>
                         <!-- /.col -->
-                        <div class="col-12">
+                        <div class="wrap-login-form-btn">
                             <button type="submit" class="btn btn-danger btn-block"><i class="far fa-arrow-alt-circle-right"></i> <b>Ingresar</b></button>
                         </div>
                         <!-- /.col -->
@@ -81,6 +89,12 @@
 
     <!-- jQuery -->
     <script src="plugins/jquery/jquery.min.js"></script>
+    <script src="plugins/jquery/jquery-3.3.1.min.js"></script>    
+    <script src="dist/js/bootstrap.min.js"></script>    
+    <script src="popper/popper.min.js"></script>    
+        
+     <script src="plugins/sweetalert2/sweetalert2.all.min.js"></script>    
+     <script src="codigo.js"></script>
     <!-- Bootstrap 4 -->
     <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- AdminLTE App -->
